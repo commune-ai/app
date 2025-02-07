a30633853a945f4eceb0aaa17b4af92a6c7a047b
Luca Vivona <lucavivona01@gmail.com>
2025-02-07 02:34:03 -0500

    update: start scripts for API and frontend

--- run/api.sh
+++ run/api.sh
@@ -1,10 +1,4 @@
 #::::::::::::::::: API :::::::::::::::::
 
 PORT=8000
-# START API
-if lsof -Pi :$PORT -sTCP:LISTEN -t >/dev/null ; then
-  echo "port $PORT is already in use"
-else
-  c serve api port=$PORT
-fi
-
+c serve api port=$PORT || echo "Failed to start API"
