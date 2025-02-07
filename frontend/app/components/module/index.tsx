996a9acc2a8f92a0606dd021c7792616c4afb555
Luca Vivona <lucavivona01@gmail.com>
2025-02-07 02:33:06 -0500

    refactor: Reorganize file layout and improve mobile responsiveness

---
+++ frontend/app/components/module/index.tsx
@@ -0,0 +1,7 @@
+import ModuleApi from "./ModuleApi";
+import ModuleCard from "./ModuleCard";
+import { CreateModule } from "./CreateModule";
+import Modules from "./Modules";
+import { ModuleCode } from "./ModuleCode";
+
+export { ModuleApi, ModuleCard, CreateModule, Modules, ModuleCode };
