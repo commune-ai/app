a39a76255bea6b969983e9cb1c384f797c17dd84
sanjipun <iamsanjipun@gmail.com>
2025-02-19 12:04:23 +0545

    sidebar bg change

--- app/components/alternate-sidebar/alternate-sidebar.tsx
+++ app/components/alternate-sidebar/alternate-sidebar.tsx
@@ -15,20 +15,16 @@ export function AlternateSidebar({ ...props }: React.ComponentProps<typeof Sideb
     <Sidebar
       collapsible="icon"
       {...props}
-      className="bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
     >
       <SidebarHeader>
-        {/* icon */}
         <span className="text-2xl font-bold text-white">
           <span className="text-blue-400">dhub</span>
         </span>
       </SidebarHeader>
       <SidebarContent>
-        {/* network and tags */}
         <p>Content</p>
       </SidebarContent>
       <SidebarFooter>
-        {/* wallet connect */}
         <WalletConnect />
       </SidebarFooter>
       <SidebarRail />
